5387170fe56e39361b28146b5f7c01d93b745c3e
chengyanan <1914293706@qq.com>
2015-11-11 14:51:30 +0800

    Initial Commit

---
+++ YNCustomTabBar/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  YNCustomTabBar
+//
+//  Created by 农盟 on 15/11/11.
+//  Copyright © 2015年 农盟. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
